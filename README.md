55dabb8a4b1f7e5b6b3440484da10fd67d9ad7fd
RayLennox <nightprowler75@hotmail.com>
2015-07-03 09:14:18 +0200

    Thread hiding
    Added readme and hiding entire threads

---
+++ README.md
@@ -0,0 +1,3 @@
+This is a userscript written in JavaScript to filter threads and posts, based on tripcodes, topics, names and posts. 
+
+Compatible with Tablecat based textboards. 
--- table filter.js
+++ table filter.js
@@ -5,7 +5,7 @@ var posts = document.getElementsByClassName('post');
 
 		for (i=0;i<posts.length;i++) {
 		if (posts[i].innerHTML.search(filt)!=-1)  {
-				posts[i].style.display="none"
+				posts[i].style.display="none";
 				
 				}
 			}
@@ -13,10 +13,14 @@ var posts = document.getElementsByClassName('post');
 
 var threads = document.getElementsByClassName('subject');
 
+var threadlist = document.getElementsByClassName('thread');
 
 for (i=0;i<threads.length;i++) {
 		if (threads[i].innerHTML.search(filt)!=-1)  {
-				threads[i].style.display="none"
-				
+				threads[i].style.display="none";
+				// hide thread in threadlist
+				threadlist[i].style.display='none';
+				//hide entire thread
+				threads[i].parentNode.parentNode.style.display='none';
 				}
 			}
